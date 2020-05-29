379a168cb72c7018505e5324e76c09422e14b196
mbacicek <33321569+mbacicek@users.noreply.github.com>
2020-05-29 10:23:30 +0200

    Add files via upload

---
+++ Row_Size.sql
@@ -0,0 +1,16 @@
+
+
+
+SELECT SCHEMA_NAME(schema_id) AS [SchemaName],
+[Tables].name AS [TableName],
+SUM([Partitions].[rows]) AS [TotalRowCount]
+FROM sys.tables AS [Tables]
+JOIN sys.partitions AS [Partitions]
+ON [Tables].[object_id] = [Partitions].[object_id]
+AND [Partitions].index_id IN ( 0, 1 )
+WHERE [Tables].name IN 
+(
+'TABLE_NAME'
+)
+GROUP BY SCHEMA_NAME(schema_id), [Tables].name;
+
